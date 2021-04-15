370a819cfb281e397ca5db92d29723c5fc1ee450
Cassandra Pamplona <cpamplona.dev@gmail.com>
2021-04-14 19:24:13 -0700

    todo list

--- src/app/list-manager/list-manager.component.ts
+++ src/app/list-manager/list-manager.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoItem } from '../interfaces/todo-item';
+import { TodoListService } from '../services/todo-list.service';
 
 @Component({
   selector: 'app-list-manager',
@@ -8,7 +9,9 @@ import { TodoItem } from '../interfaces/todo-item';
     <app-input-button-unit (submit)="addItem($event)"></app-input-button-unit>
     <ul>
         <li *ngFor="let todoItem of todoList">
-          <app-todo-item [item]="todoItem"></app-todo-item>
+          <app-todo-item [item]="todoItem"
+                        (remove)="removeItem($event)"
+                        (update)="updateItem($event.item, $event.changes)"></app-todo-item>
         </li>
       </ul>
   </div>
@@ -16,22 +19,24 @@ import { TodoItem } from '../interfaces/todo-item';
   styleUrls: ['./list-manager.component.scss']
 })
 export class ListManagerComponent implements OnInit {
-
-  constructor() { }
+  todoList: TodoItem[];
+  constructor(private todoListService: TodoListService) {
+  }
 
   ngOnInit(): void {
+    this.todoList = this.todoListService.getTodoList();
   }
-  todoList: TodoItem[] = [
-    {title: 'install NodeJS'},
-    {title: 'install Angular CLI'},
-    {title: 'create new app'},
-    {title: 'serve app'},
-    {title: 'develop app'},
-    {title: 'deploy app'},
-  ];
 
-  addItem(title: string) {
-    this.todoList.push({ title });
+  addItem(title: string): void {
+    this.todoListService.addItem({ title });
+  }
+
+  removeItem(item): void {
+    this.todoListService.deleteItem(item);
+  }
+
+  updateItem(item, changes): void {
+    this.todoListService.updateItem(item, changes);
   }
 
 }
--- src/app/todo-item/todo-item.component.ts
+++ src/app/todo-item/todo-item.component.ts
@@ -1,20 +1,43 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { TodoItem } from '../interfaces/todo-item';
 
 @Component({
   selector: 'app-todo-item',
   template: `
     <div class="todo-item">
-      {{ item.title }}
+    <input type="checkbox"
+         class="todo-checkbox"
+         (click)="completeItem()"
+         [checked]="item.completed"/>
+         <span class="todo-title" [ngClass]="{'todo-complete': item.completed}">
+          {{ item.title }}
+        </span>
+      <button class="btn btn-red" (click)="removeItem()">
+      remove
+    </button>
     </div>
   `,
   styleUrls: ['./todo-item.component.scss']
 })
 export class TodoItemComponent implements OnInit {
   @Input() item: TodoItem;
+  @Output() remove: EventEmitter<TodoItem> = new EventEmitter<TodoItem>();
+  @Output() update: EventEmitter<any> = new EventEmitter<any>();
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  completeItem(): void {
+    this.update.emit({
+      item: this.item,
+      changes: {completed: !this.item.completed}
+    });
+  }
+
+  removeItem(): void {
+    this.remove.emit(this.item);
+  }
+
 
 }
